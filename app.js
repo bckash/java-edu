7725d630d118ff0aaec40da263d759bb403fbff6
bckash <59818049+bckash@users.noreply.github.com>
2020-03-06 05:56:12 +0000

    img src change
    button that changes src of the imges looping that

--- app.js
+++ app.js
@@ -25,7 +25,9 @@ const par2 = document.getElementById("d2");
 const hid2 = document.getElementById("h2");
 
 but2.addEventListener("click", function(){
-    par2.innerHTML = `<a href="https://www.nike.com/gb/" target="_blank">nike<a/>`
+    par2.innerHTML = `<a href="https://www.nike.com/gb/" target="_blank">
+                        <img id="nike" src="img/nike.png">
+                        <a/>`
 });
 
 //Click & hide: showing text. using "toggle" method from classList property
@@ -37,4 +39,24 @@ const hid3 = document.getElementById("h3");
 but3.addEventListener("click", function(){
     par3.innerHTML = "Hello!"
     par3.classList.toggle("d3");
+});
+
+// TESLA - changing img src. For the EL function - the trick is to asign number to each picture situation, with initial value assigned to "n outside the function, so it is available everytime the function starts (#scope!, #clousures!). If the "n" was in the function it would got "destroyed" everytime the function starts (it would have the same value in the begginig everytimne) 
+
+const butColor = document.getElementById("color_change");
+const teslaImg = document.getElementById("tesla_pick");
+
+let n=0;
+
+butColor.addEventListener("click", function(){
+
+        if (n==0) {
+            teslaImg.src = "img/tes_wh.jpg";
+            n = 1;}
+
+        else {
+            teslaImg.src = "img/tes_bl.jpg";
+            n = 0;}
+    
+
 });
